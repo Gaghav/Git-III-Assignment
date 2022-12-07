2f37a30d25c6f7809ff62c20ba8ac6d116602f51
Gajanan Aghav <aghao8806@gmail.com>
2022-12-07 08:40:09 +0530

    code fixed for prime

--- prime.js
+++ prime.js
@@ -1,10 +1,23 @@
-function checkprime(num){
-     for (let i=i; i<=num; i++)
+function checkprime(number){
+       let factors=0;
+     for (let i=1; i<=number; i++)
+     { if (number%i==0){
+           factors++;
+       }
+      if (factors==2)
      {
-           return true;
+          return true;
+     }
+
      }
      return false;
 
 
 }
-checkprime (13);
+lrt ansecheckprime (13);
+
+if (answer==true){
+console.log("prime number");
+}else{
+          console.log("not a prime number");
+}
